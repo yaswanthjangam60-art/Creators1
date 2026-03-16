6886d7ddfcb8dd5ea49c83396a89e5abcef88fca
yaswanthjangam60-art <yaswanthjangam60@gmail.com>
2026-03-16 14:58:31 +0530

    Update supabase.js

--- Creators/supabase.js
+++ Creators/supabase.js
@@ -1,11 +1,11 @@
 const client = window.supabase.createClient(
-"https://vhdtlbpmstvnpcikrcwo.supabase.co",
-"sb_publishable_Rs5RaSAA2o3OXbWTL4-e2Q_V8kfzg0y",
-{
-auth:{
-persistSession:true,
-autoRefreshToken:true,
-storage:window.localStorage
-}
-}
+  "https://vhdtlbpmstvnpcikrcwo.supabase.co",
+  "sb_publishable_Rs5RaSAA2o3OXbWTL4-e2Q_V8kfzg0y",
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      storage: window.localStorage
+    }
+  }
 )
