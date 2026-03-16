91b4bceef9c3e76ab0034e7051a6fb68dc43b7b2
yaswanthjangam60-art <yaswanthjangam60@gmail.com>
2026-03-16 23:07:45 +0530

    Update test.js

--- Creators/api/test.js
+++ Creators/api/test.js
@@ -1,3 +1,11 @@
 export default function handler(req, res) {
-  res.json({ ok: true, env_url: !!process.env.SUPABASE_URL, env_key: !!process.env.SUPABASE_SERVICE_KEY, env_apify: !!process.env.APIFY_TOKEN })
+
+  const SUPABASE_SERVICE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZHRsYnBtc3R2bnBjaWtyY3dvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzU3MzIxMCwiZXhwIjoyMDg5MTQ5MjEwfQ.W4X_Bw9NWHJFCC_zSEE4O-8Ooz-hE15nPndUOAJ_OfE"
+
+  res.json({
+    ok: true,
+    env_url: !!process.env.SUPABASE_URL,
+    env_key: !!SUPABASE_SERVICE_KEY,
+    env_apify: !!process.env.APIFY_TOKEN
+  })
 }
