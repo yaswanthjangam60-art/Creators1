c060f1cce29dbb4dee603f69e6c4cbe73380c14e
yaswanthjangam60-art <yaswanthjangam60@gmail.com>
2026-03-16 23:14:11 +0530

    Update test.js

--- Creators/api/test.js
+++ Creators/api/test.js
@@ -1,10 +1,9 @@
 export default function handler(req, res) {
 
-  
   res.json({
     ok: true,
     env_url: !!process.env.SUPABASE_URL,
-    env_key: !!SUPABASE_SERVICE_KEY,
+    env_key: !!process.env.SUPABASE_SERVICE_KEY,
     env_apify: !!process.env.APIFY_TOKEN
   })
 }
