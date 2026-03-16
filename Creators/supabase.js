2f047418b769ceee5377dff1000a2bb89c9402c1
yaswanthjangam60-art <yaswanthjangam60@gmail.com>
2026-03-16 20:55:11 +0530

    Update supabase.js

--- Creators/supabase.js
+++ Creators/supabase.js
@@ -1,6 +1,6 @@
 const client = window.supabase.createClient(
   "https://vhdtlbpmstvnpcikrcwo.supabase.co",
-  "sb_publishable_Rs5RaSAA2o3OXbWTL4-e2Q_V8kfzg0y",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZHRsYnBtc3R2bnBjaWtyY3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NzMyMTAsImV4cCI6MjA4OTE0OTIxMH0.1SwUzf7UAyUslpxIk6x7rXLfa4-naElyNSWtI2Un570",
   {
     auth: {
       persistSession: true,
