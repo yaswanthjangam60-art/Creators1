f4039b0487a1745af1b114563c956e25cbb23e91
yaswanthjangam60-art <yaswanthjangam60@gmail.com>
2026-03-16 22:37:06 +0530

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.json({ ok: true, env_url: !!process.env.SUPABASE_URL, env_key: !!process.env.SUPABASE_SERVICE_ROLE_KEY, env_apify: !!process.env.APIFY_TOKEN })
+}
