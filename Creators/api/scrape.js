e5d35b7112b3ffe66a1d6df78a492143ce2f0372
yaswanthjangam60-art <yaswanthjangam60@gmail.com>
2026-03-16 17:52:41 +0530

    Update scrape.js

--- Creators/api/scrape.js
+++ Creators/api/scrape.js
@@ -1,17 +1,35 @@
 export default async function handler(req, res) {
 
-const APIFY_URL = "https://api.apify.com/v2/datasets/8gR8RUynFc75jD562/items?token=apify_api_gNQRi79dA204kpuWOQdzKDSexLbYZu005pM8"
+const username = req.query.username
+
+if (!username) {
+return res.status(400).json({ error: "username required" })
+}
+
+const APIFY_TOKEN = "https://api.apify.com/v2/datasets/8gR8RUynFc75jD562/items?token=apify_api_gNQRi79dA204kpuWOQdzKDSexLbYZu005pM8"
+
+const APIFY_URL = `https://api.apify.com/v2/acts/apify~instagram-profile-scraper/run-sync-get-dataset-items?token=${APIFY_TOKEN}`
 
 try {
 
-const response = await fetch(APIFY_URL)
+const response = await fetch(APIFY_URL, {
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+usernames: [username],
+resultsLimit: 20
+})
+})
+
 const data = await response.json()
 
 res.status(200).json(data)
 
-} catch(err) {
+} catch (err) {
 
-res.status(500).json({error:"scraping failed"})
+res.status(500).json({ error: "scraping failed" })
 
 }
 
